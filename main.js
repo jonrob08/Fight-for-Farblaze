d371531b1bc699d933fa69fbd1d233221bd3977f
Jon R <jonrob08@gmail.com>
2022-07-25 18:45:15 -0400

    Added Hitboxes
    Added some comments to the code, as well as a hitbox property that's an object that sits on top of the player and extends out depending on how large the attack range is. For now it's just a solid 100px, also I'm drawing the hitBox out in the draw method so I can visualize it better. Also started code for collision detection, it's now working on the x axis.

--- main.js
+++ main.js
@@ -49,6 +49,10 @@ const keys = {
 
         Width - Need this to refer to the width of Player on x axis
 
+        lastKey - Need this to differentiate keys that are being held down, as multiple keys can be pressed down at once we also want to check which key was held down last. We instantiate it as a falsy value because when we press a key in the EventListener it becomes a string of that key which we can use later.
+
+        hitBox - Need this common property to determine if a player or enemy was hit or not. Each Player has an attack so if that crosses another persons hitbox they will be hit. hitBox is an object that has a position that is the Player's position, a width of 100 (subject to change depending on attack range and how bold I'm feeling), and a height of 30 (also subject to change).
+
     Methods - 
         Draw() - For reference I am drawing a rectangle placeholder for my Player 1 and Player 2. I am using a fillStyle to make my rectangle blue
 
@@ -61,14 +65,23 @@ class Player {
     constructor({position, velocity}) {
         this.position = position
         this.velocity = velocity
-        this.height = 100
+        this.height = 150
         this.width = 50
         this.lastKey
+        this.hitBox = {
+            position: this.position,
+            width: 100,
+            height: 50
+        }
     }
 
     draw() {
         ctx.fillStyle = 'blue'
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        // This is where the hitBox is drawn
+        ctx.fillStyle = 'red'
+        ctx.fillRect(this.hitBox.position.x, this.hitBox.position.y, this.hitBox.width, this.hitBox.height)
     }
 
     update() {
@@ -76,6 +89,7 @@ class Player {
 
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
+
         if (this.position.y + this.height > canvas.height) {
             this.velocity.y = 0
         } else this.velocity.y += gravity
@@ -108,30 +122,60 @@ const playerTwo = new Player({
     }
 })
 
+// Creating Enemy
+const enemy = new Player({
+    position: {
+        x: 700,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
 /** Animate function - This recursive function "animates" the canvas in our browser window by calling itself and refreshes the frame by 
 
     -Reference: https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
  */ 
 function animate() {
+    // Call window.requestAnimationFrame() and pass in animate to refresh the canvas constantly
     window.requestAnimationFrame(animate)
+    // Set the fill style to black
     ctx.fillStyle = 'black'
+    // Fill the entire area of the canvas with a black rectangle
     ctx.fillRect(0, 0, canvas.width, canvas.height)
+    // Draw and animate player 1
     playerOne.update()
+    // Draw and animate player 2
     playerTwo.update()
-
+    // Draw and animate enemy
+    enemy.update()
+    // Set each player's velocity to 0 
     playerOne.velocity.x = 0
     playerTwo.velocity.x = 0
 
+    // Player 1 Movement
     if (keys.a.pressed && playerOne.lastKey === 'a') {
         playerOne.velocity.x = -2
     } else if (keys.d.pressed && playerOne.lastKey === 'd') {
         playerOne.velocity.x = 2
     }
 
+    // Player 2 Movement
     if (keys.ArrowRight.pressed && playerTwo.lastKey === 'ArrowRight') {
         playerTwo.velocity.x = 2
     } else if (keys.ArrowLeft.pressed && playerTwo.lastKey === 'ArrowLeft') {
         playerTwo.velocity.x = -2
+    }
+
+    // Collision Detection
+    if (playerOne.hitBox.position.x + playerOne.hitBox.width >= enemy.position.x && playerOne.hitBox.position.x <= enemy.position.x + enemy.width) {
+        console.log('Hit!')
+    }
+
+    if (playerTwo.hitBox.position.x + playerTwo.hitBox.width >= enemy.position.x && playerTwo.hitBox.position.x <= enemy.position.x + enemy.width) {
+        console.log('Hit!')
     }
 }
 
